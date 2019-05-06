dd3dfeedaa6b3adef630ed640f9ac039276410bb
Erik Johansson <erik.johansson@capo.local>
2019-05-06 13:27:17 +0200

    fixed json productPage

--- vuexstore/src/components/Products/ProductPage.vue
+++ vuexstore/src/components/Products/ProductPage.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="productspage">
-      <h1 class="ppage">PRODUCT PAGE</h1>
+      <h1 class="ppage">PRODUCT PAGE  </h1>
+      <ul class="productsUl">
+        <li v-for="item in items" :key="item.name">
+            <p>{{item.price}}</p>
+        </li>
+        
 
+        </ul>
   </div>
 </template>
 
@@ -11,15 +17,28 @@
 <script>
 //styles
 import productsStyles from './styles/productPageStyles.css';
+import axios from 'axios'
 
 
 export default {
   name: 'ProductPage',
   data() {
-      return {
-      
+    return {
+      items: []
       }
-     }
+     },
+  mounted () {
+    var self = this;
+    axios
+      .get("products.json")
+      .then(response => (this.items = response.data))
+     
+    
+
+     
+
+
+  }
 }
 </script>
 
