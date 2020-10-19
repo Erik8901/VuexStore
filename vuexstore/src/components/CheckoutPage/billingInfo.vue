7cebe45274b056eb058e06c474f41658818f9eae
Erik <erikjohansson890@gmail.com>
2020-10-19 13:02:38 +0200

    checkout info working

--- vuexstore/src/components/CheckoutPage/billingInfo.vue
+++ vuexstore/src/components/CheckoutPage/billingInfo.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="billing-info">
-     <h1>helo</h1>
-     <input />
+    <div class="billing-text">
+      <span class="contact-title">Contact Information</span>
+        <div class="account-text">
+          <span class="accountTitle">Already have an acccount?</span>
+          <span class="loginTitle">Log in</span>
+      </div>
+    </div>
+      <div class="inputFields">
+        <div class="userInputs">
+          <input class="input-email" placeholder="Email" type="text" />
+            <div class="checkbox-div">
+            <input type="checkbox" class="checkbox" value="subscribe" />
+            <span class="subscribe-text">Keep me up to date with news and special offers</span>
+          </div>
+        <div class="personal-info-container">
+            <span class="personal-title">Personal Information</span>
+              <div class="names-container">
+                <input class="input-names" placeholder="First Name" type="text" />
+                <input class="input-names" placeholder="Last Name" type="text" />
+              </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
