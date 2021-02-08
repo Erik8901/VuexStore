bbcdd9c7dc121385adea89be6f8c0e128badbec8
Erik <erikjohansson890@gmail.com>
2021-02-08 13:35:21 +0100

    working on header

--- vuexstore/src/components/HeaderTop/HeaderTop.vue
+++ vuexstore/src/components/HeaderTop/HeaderTop.vue
@@ -1,34 +1,38 @@
 <template>
   <div class="headerTop">
-    <div class="container">
-      <router-link to="/"><h3 class="companyName">Kitchen Equipment</h3></router-link>
-        <searchBar class="searchBar"></searchBar>
+      <div class="header-container">
+            <div class="company-name-container">
+                <router-link to="/"><span class="companyName">Kitchen Equipment</span></router-link>
+            </div>
+            <div class="search-bar-container">
+                <searchBar class="searchBar"></searchBar>
+            </div>
+          
           <div class="cart-and-checkout">
-          <i class="fas fa-shopping-cart"></i>
-          <router-link to="/checkoutPage">
-              <span class="mobileCartAmount">({{this.productsCart.length}})</span>
-           </router-link>
+              <i class="fas fa-shopping-cart"></i>
+                  <router-link to="/checkoutPage">
+                      <span class="mobileCartAmount">({{this.productsCart.length}})</span>
+                  </router-link>
 
-            <div class="empty-cart" v-if="!this.productsCart.length" @mouseover="checkIfCartIsEmpty()" @mouseleave="mouseleave()" >
-              <div class="if-empty" >Your cart is empty</div>
-                <span to="/checkoutPage" class="link-checkout"  >
-                  <span class="checkout-text">To Checkout (<span class="cartAmount" >{{this.productsCart.length}}</span>)
-                </span>
-              </span>
+              <div class="empty-cart" v-if="!this.productsCart.length" @mouseover="checkIfCartIsEmpty()" @mouseleave="mouseleave()" >
+                  <div class="if-empty" >Your cart is empty</div>
+                      <span to="/checkoutPage" class="link-checkout" >
+                      <span class="checkout-text">To Checkout (<span class="cartAmount" >{{this.productsCart.length}}</span>)
+                      </span>
+                      </span>
+                  </div>
+
+              <div class="cart-div" v-if="this.productsCart.length"  >
+                  <router-link to="/checkoutPage" class="link-checkout"  >
+                      <span class="checkout-text">To Checkout (<span class="cartAmount" >{{this.productsCart.length}}</span>)
+                      </span>
+                    </router-link>
               </div>
-
-          <div class="cart-div" v-if="this.productsCart.length"  >
-              <router-link to="/checkoutPage" class="link-checkout"  >
-                  <span class="checkout-text">To Checkout (<span class="cartAmount" >{{this.productsCart.length}}</span>)
-                </span>
-              </router-link>
-            </div>
-            
-            </div>
-          <div class="loginButtonsDiv">
-            <button class="buttonsCustomers">Login</button>
-            <button class="buttonsCustomers">Register</button>
-    </div>
+          </div>
+              <div class="loginButtonsDiv">
+                  <button class="buttonsCustomers">Login</button>
+                  <button class="buttonsCustomers">Register</button>
+              </div>
     </div>
   </div>
 </template>
