483d2e85b5a8cedb73b40826f11a9a0f29acf84c
Erik <erikjohansson890@gmail.com>
2021-04-12 14:47:42 +0200

    working on payments with cards

--- vuexstore/src/components/CheckoutPage/StripeCheckout.vue
+++ vuexstore/src/components/CheckoutPage/StripeCheckout.vue
@@ -1,47 +1,48 @@
 <template>
-  <div class="payment-container">
-    helo
-    <stripe-checkout
-      ref="checkoutRef"
-      mode="payment"
-      :pk="publishableKey"
-      :line-items="lineItems"
-      :success-url="successURL"
-      :cancel-url="cancelURL"
-      @loading="v => loading = v"
-    />
-    <button @click="submit">Pay now!</button>
+  <div class="stripe-checkout-page">
+    <h1 class="billing-title">COMPLETE ORDER</h1>
+    <div class="payment-container">
+      <stripe-checkout
+        ref="checkoutRef"
+        mode="payment"
+        :pk="publishableKey"
+        :line-items="lineItems"
+        :success-url="successURL"
+        :cancel-url="cancelURL"
+        @loading="(v) => (loading = v)"
+      />
+      <button @click="submit">Pay now!</button>
+    </div>
   </div>
 </template>
 
 <script>
-import { StripeCheckout } from '@vue-stripe/vue-stripe';
+import { StripeCheckout } from "@vue-stripe/vue-stripe";
 
 //styles
-import stylesStripCheckout from './styles/stylesStripCheckout.css';
-
+import stylesStripCheckout from "./styles/stylesStripCheckout.css";
 
 export default {
   components: {
     StripeCheckout,
   },
-  data () {
+  data() {
     this.publishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
     return {
       loading: false,
       lineItems: [
         {
-          price: 'price_1IOL5SB9QUYkdDctEQsdTdF2', // The id of the one-time price you created in your Stripe dashboard
+          price: "prod_J0LnnYdWYlgCnJ", // The id of the one-time price you created in your Stripe dashboard
           quantity: 1,
         },
       ],
-      successURL: 'your-success-url',
-      cancelURL: 'your-cancel-url',
+      successURL: "your-success-url",
+      cancelURL: "your-cancel-url",
     };
   },
   // You will be redirected to Stripe's secure checkout page
-  submit () {
-    console.log("hereqz")
+  submit() {
+    console.log("hereqz");
     this.$refs.checkoutRef.redirectToCheckout();
   },
 };
