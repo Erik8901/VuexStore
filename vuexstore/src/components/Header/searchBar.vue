61fc1d386d0a84193b47dee5c4b3846300d23814
Erik Ojeda Johansson <erikjohansson890@gmail.com>
2022-07-25 13:09:08 +0200

    minor css changes

--- vuexstore/src/components/Header/searchBar.vue
+++ vuexstore/src/components/Header/searchBar.vue
@@ -1,89 +1,79 @@
 <template>
   <div class="search-container">
-      <input placeholder="Search our webstore..." class="searchField"
+    <input
+      placeholder="Search our webstore..."
+      class="searchField"
       type="text"
       @keyup="loadSearchPage"
       v-model="keyWords"
-      
     />
-      <div class="div-searchIcon">
-        <i class="fas fa-search"></i>
-      </div>
-      <!-- <img src="/img/searchIcon.png" /> -->
-     <!-- <SearchPage/> -->
+    <div class="div-searchIcon">
+      <i class="fas fa-search"></i>
+    </div>
+    <!-- <img src="/img/searchIcon.png" /> -->
+    <!-- <SearchPage/> -->
   </div>
 </template>
 
 <script>
 //styles
-import headerStyles from './styles/headerStyles.css';
-import ProductPage from '../Products/ProductPage.vue'
+import headerStyles from "./styles/headerStyles.css";
+import ProductPage from "../Products/ProductPage.vue";
 import { mapGetters } from "vuex";
 import store from "@/utils/store.js";
-import SearchPage from '../Products/SearchPage.vue'
+import SearchPage from "../Products/SearchPage.vue";
 
 export default {
-  name: 'searchBar',
+  name: "searchBar",
   components: {
-    SearchPage
+    SearchPage,
   },
   computed: {
     items() {
-      return this.$store.state.items
-    }
-
+      return this.$store.state.items;
+    },
   },
   data() {
     return {
-      keyWords: '',
-      viewWidth: 0
-    }
-},
-created() {
-      window.addEventListener('resize', this.handleWindowSize)
+      keyWords: "",
+      viewWidth: 0,
+    };
   },
-methods: {
-    loadSearchPage: function () {
+  created() {
+    window.addEventListener("resize", this.handleWindowSize);
+  },
+  methods: {
+    loadSearchPage: function() {
+      console.log("test");
+      let minKeys = 3;
 
-      console.log("test")
-    let minKeys = 3
-   
-  setTimeout(() =>{
-    if(this.keyWords.length >= minKeys) {
-      
-       this.$store.commit('SET_SEARCH', {
-        searchWords: this.keyWords,
-        allItems: this.items
-      })
-      
-      this.keyWords = ''
-      this.$router.push({ path: '/SearchPage' })
-   
-      }
-    }, 2000)
-  }, //loadSearchPage
+      setTimeout(() => {
+        if (this.keyWords.length >= minKeys) {
+          this.$store.commit("SET_SEARCH", {
+            searchWords: this.keyWords,
+            allItems: this.items,
+          });
 
-  handleWindowSize() {
+          this.keyWords = "";
+          this.$router.push({ path: "/SearchPage" });
+        }
+      }, 2000);
+    }, //loadSearchPage
+
+    handleWindowSize() {
       // this.viewWidth = window.innerWidth;
-        
       //   let searchField = document.querySelector(".searchField");
       //   let searchIcon = document.querySelector(".searchIcon");
-
-      //   this.viewWidth < 1300 ? 
+      //   this.viewWidth < 1300 ?
       //     searchField.style.left = "-17%" :
       //     searchField.style.left = "31%"
-
-      //   this.viewWidth < 1300 ? 
+      //   this.viewWidth < 1300 ?
       //     searchIcon.style.left = "-17%" :
       //     searchIcon.style.left = "31%"
-
-        // this.viewWidth < 1300 ? 
-        //   searchField.style.width = "10rem" :
-        //   searchField.style.width = "16rem"
-  
-    }
-  } 
-}
+      // this.viewWidth < 1300 ?
+      //   searchField.style.width = "10rem" :
+      //   searchField.style.width = "16rem"
+    },
+  },
+};
 </script>
-
-
